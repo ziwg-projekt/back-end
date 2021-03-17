d0e2e3e63cf751e9d9bf37151a858d4c9af391b8
Krystian Kania <242014@student.pwr.edu.pl>
2021-03-17 23:11:02 +0100

    Add simple Hospital entity

--- src/main/java/pl/ziwg/backend/model/entity/Address.java
+++ src/main/java/pl/ziwg/backend/model/entity/Address.java
@@ -5,7 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "address")
@@ -17,6 +24,9 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private long ID;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "address")
+    private Hospital hospital;
 
     private String city;
     private String street;
