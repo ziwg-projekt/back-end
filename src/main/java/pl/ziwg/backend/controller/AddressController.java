0bf026b793b59eebccef0ba48fbe79db951fab37
radosz99 <47248979+radosz99@users.noreply.github.com>
2021-03-19 16:17:02 +0100

    Upgrade responses with Spring Hateoas style

--- src/main/java/pl/ziwg/backend/controller/AddressController.java
+++ src/main/java/pl/ziwg/backend/controller/AddressController.java
@@ -1,25 +1,83 @@
 package pl.ziwg.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.ziwg.backend.model.entity.Address;
+import pl.ziwg.backend.model.modelassembler.AddressModelAssembler;
 import pl.ziwg.backend.service.AddressService;
+import pl.ziwg.exception.AddressNotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 @RestController
-@RequestMapping("/api/v1/address")
+@RequestMapping("/api/v1/addresses")
 public class AddressController {
     private AddressService addressService;
+    private AddressModelAssembler assembler;
 
     @Autowired
-    public AddressController(AddressService addressService) {
+    public AddressController(AddressService addressService, AddressModelAssembler assembler) {
         this.addressService = addressService;
+        this.assembler = assembler;
     }
 
-    @GetMapping("/get-all")
-    public Iterable<Address> getAll() {
-        return addressService.findAll();
+    @GetMapping("")
+    public CollectionModel<EntityModel<Address>> getAll() {
+
+        List<EntityModel<Address>> employees = addressService.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        
+        return CollectionModel.of(employees, linkTo(methodOn(AddressController.class).getAll()).withSelfRel());
     }
+
+    @PostMapping("")
+    public ResponseEntity<?> newAddress(@RequestBody Address newAddress) {
+
+        EntityModel<Address> entityModel = assembler.toModel(addressService.save(newAddress));
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+                .body(entityModel);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> replaceAddress(@RequestBody Address newAddress, @PathVariable Long id) {
+
+        Address updatedEmployee = addressService.findById(id)
+                .map(address -> {
+                    address.setLatitude(newAddress.getLatitude());
+                    address.setLongitude(newAddress.getLongitude());
+                    return addressService.save(address);
+                })
+                .orElseGet(() -> {
+                    newAddress.setId(id);
+                    return addressService.save(newAddress);
+                });
+
+        EntityModel<Address> entityModel = assembler.toModel(updatedEmployee);
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+    }
+
+    @GetMapping("/{id}")
+    public EntityModel<Address> getOne(@PathVariable Long id) {
+        Address address = addressService.findById(id)
+                .orElseThrow(() -> new AddressNotFoundException(id));
+
+        return assembler.toModel(address);
+    }
+
 }
