ee67be6791c9651202d2f6d5ca8e7203c362d486
radosz99 <47248979+radosz99@users.noreply.github.com>
2021-03-21 19:03:04 +0100

    Add Vaccine entity

--- src/main/java/pl/ziwg/backend/exception/AddressNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package pl.ziwg.backend.exception;
-
-public class AddressNotFoundException extends RuntimeException {
-
-    public AddressNotFoundException(Long id) {
-        super("Could not find address " + id);
-    }
-}
