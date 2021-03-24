c031cce05ad926b0d06f7f71a7ea17387ec7b5f4
radosz99 <47248979+radosz99@users.noreply.github.com>
2021-03-24 21:23:26 +0100

    Add CrossOrigin annotation in auth controller

--- src/main/java/pl/ziwg/backend/controller/AuthenticationController.java
+++ src/main/java/pl/ziwg/backend/controller/AuthenticationController.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
