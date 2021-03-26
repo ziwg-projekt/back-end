70ba9303b8f9ee7c4a3db143ba6938d73f90edbe
radosz99 <47248979+radosz99@users.noreply.github.com>
2021-03-26 20:42:42 +0100

    Add interface for EmailSender

--- src/main/java/pl/ziwg/backend/notificator/email/EmailNotificator.java
+++
@@ -1,7 +0,0 @@
-package pl.ziwg.backend.notificator.email;
-
-public class EmailNotificator {
-    public void sendNotification(String email, String message){
-        //TODO: whole logic
-    }
-}
---
+++ src/main/java/pl/ziwg/backend/notificator/email/EmailSender.java
@@ -0,0 +1,8 @@
+package pl.ziwg.backend.notificator.email;
+
+public class EmailSender implements IEmailSender {
+    @Override
+    public void sendMail(String mail, String message) {
+        //TODO: whole logic
+    }
+}
