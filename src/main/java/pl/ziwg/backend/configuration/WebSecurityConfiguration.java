3d1ac3044c8eb51baed6ff750ca0b7625e9a00a3
radosz99 <radoslaw.lis@nokia.com>
2021-04-04 19:24:58 +0200

    Add endpoint for getting user by id

--- src/main/java/pl/ziwg/backend/configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/ziwg/backend/configuration/WebSecurityConfiguration.java
@@ -89,6 +89,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter imple
             "/swagger-ui/**",
             "/api/v1/auth/**",
             "/api/v1/hospitals/**",
+            "/api/v1/users/**",
             "/api/v1/companies/**"
             // other public endpoints of your API may be appended to this array
     };
