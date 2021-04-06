5fbde640d55aa51c309aca62fb7fc66087d679a6
Krystian Kania <242014@student.pwr.edu.pl>
2021-04-06 23:37:32 +0200

    Add sms service for tests

--- src/main/java/pl/ziwg/backend/notificator/sms/SMSSender.java
+++ src/main/java/pl/ziwg/backend/notificator/sms/SMSSender.java
@@ -1,7 +1,34 @@
 package pl.ziwg.backend.notificator.sms;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Service
 public class SMSSender {
-    public void sendMessage(String phoneNumber, String message){
-        //TODO: whole logic
+    @Value("${sms.account.sid}")
+    private String accountSid;
+
+    @Value("${sms.auth.id}")
+    private String authId;
+
+    @Value("${sms.number}")
+    private String myPhoneNumber;
+
+    @Async
+    public void sendMessage(@NotNull final String phoneNumber, @NotNull final String message) {
+        if (Objects.nonNull(accountSid) && Objects.nonNull(authId)) {
+            Twilio.init(accountSid, authId);
+        } else {
+            throw new RuntimeException("account SID or Auth id were not loaded");
+        }
+
+        Message.creator(new PhoneNumber(phoneNumber), new PhoneNumber(myPhoneNumber), message).create();
     }
 }
