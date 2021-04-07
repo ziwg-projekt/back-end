4d77ce082bcc527f2964e5ea86be4cbb5131bb8e
Krystian Kania <242014@student.pwr.edu.pl>
2021-04-07 23:03:04 +0200

    Add logic to SMSService

--- src/main/java/pl/ziwg/backend/service/SMSService.java
+++ src/main/java/pl/ziwg/backend/service/SMSService.java
@@ -1,17 +1,31 @@
 package pl.ziwg.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.ziwg.backend.notificator.sms.SMSSender;
 
 @Service
 public class SMSService {
-    private SMSSender smsSender = new SMSSender();
+    private SMSSender smsSender;
 
-    public void sendVerificationCode(String number, String message){
-        //TODO: whole logic
+    @Autowired
+    public SMSService(SMSSender smsSender) {
+        this.smsSender = smsSender;
     }
 
-    public void sendPasswordReminder(String number, String message){
-        //TODO: whole logic
+    public void sendVerificationCode(String number, String verificationCode) {
+        String message = String.format("Kod weryfikacyjny: %s", verificationCode);
+        smsSender.sendMessage(number, message);
+    }
+
+    public void sendPasswordReminder(String number, String passwordReminder) {
+        String message = String.format("Wpisz dany kod na naszej stronie internetowej w celu zmiany danych: %s",
+                passwordReminder);
+        smsSender.sendMessage(number, message);
+    }
+
+    public void sendVaccinationDate(String number, String vaccinationDate) {
+        String message = String.format("Termin szczepienia: %s", vaccinationDate);
+        smsSender.sendMessage(number, message);
     }
 }
