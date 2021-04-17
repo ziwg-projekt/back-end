58a6f884754dbb310c3993253a2ef37297c6d912
Krystian Kania <242014@student.pwr.edu.pl>
2021-04-18 01:27:24 +0200

    Add endpoint to update citizen data, for now without validation

--- src/main/java/pl/ziwg/backend/service/CitizenService.java
+++ src/main/java/pl/ziwg/backend/service/CitizenService.java
@@ -1,14 +1,19 @@
 package pl.ziwg.backend.service;
 
+import org.hibernate.validator.constraints.pl.PESEL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import pl.ziwg.backend.dto.CitizenUpdateDto;
+import pl.ziwg.backend.dto.CitizenUpdateResponseDto;
+import pl.ziwg.backend.exception.ResourceNotFoundException;
 import pl.ziwg.backend.model.entity.Citizen;
 import pl.ziwg.backend.model.repository.CitizenRepository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Transactional
@@ -21,11 +26,11 @@ public class CitizenService {
         this.citizenRepository = citizenRepository;
     }
 
-    public Page<Citizen> findAllFromPage(Pageable pageable){
+    public Page<Citizen> findAllFromPage(Pageable pageable) {
         return citizenRepository.findAll(pageable);
     }
 
-    public List<Citizen> findAll(){
+    public List<Citizen> findAll() {
         return citizenRepository.findAll();
     }
 
@@ -37,12 +42,54 @@ public class CitizenService {
         return citizenRepository.save(citizen);
     }
 
-    public void deleteCitizen(String pesel){
+    public void deleteCitizen(String pesel) {
         citizenRepository.deleteByPesel(pesel);
     }
 
-    public boolean checkIfExistsByPesel(String pesel){
+    public boolean checkIfExistsByPesel(String pesel) {
         return citizenRepository.existsByPesel(pesel);
+    }
+
+    public CitizenUpdateResponseDto updateCitizenData(final CitizenUpdateDto citizenUpdateDto, @PESEL final String pesel) {
+        final Citizen citizen = citizenRepository.findByPesel(pesel)
+                .orElseThrow(() -> new ResourceNotFoundException(pesel, "citizen"));
+        if (Objects.nonNull(citizenUpdateDto.getName())) {
+            citizen.setName(citizenUpdateDto.getName());
+        }
+        if (Objects.nonNull(citizenUpdateDto.getSurname())) {
+            citizen.setSurname(citizenUpdateDto.getSurname());
+        }
+        if (Objects.nonNull(citizenUpdateDto.getAddress())) {
+            citizen.setAddress(citizenUpdateDto.getAddress());
+        }
+        if (Objects.nonNull(citizenUpdateDto.getEmail())) {
+            citizen.setEmail(citizenUpdateDto.getEmail());
+        }
+        if (Objects.nonNull(citizenUpdateDto.getPhoneNumber())) {
+            citizen.setPhoneNumber(citizenUpdateDto.getPhoneNumber());
+        }
+        if (Objects.nonNull(citizenUpdateDto.getState())) {
+            citizen.setState(citizenUpdateDto.getState());
+        }
+        if (Objects.nonNull(citizenUpdateDto.getHospital())) {
+            citizen.setHospital(citizenUpdateDto.getHospital());
+        }
+        citizenRepository.save(citizen);
+        return mapFrom(citizen);
+    }
+
+    private CitizenUpdateResponseDto mapFrom(final Citizen citizen) {
+        final CitizenUpdateResponseDto citizenUpdateResponseDto = new CitizenUpdateResponseDto();
+        citizenUpdateResponseDto.setName(citizen.getName());
+        citizenUpdateResponseDto.setSurname(citizen.getSurname());
+        citizenUpdateResponseDto.setEmail(citizen.getEmail());
+        citizenUpdateResponseDto.setAddress(citizen.getAddress());
+        citizenUpdateResponseDto.setHospital(citizen.getHospital());
+        citizenUpdateResponseDto.setPhoneNumber(citizen.getPhoneNumber());
+        citizenUpdateResponseDto.setState(citizen.getState());
+        citizenUpdateResponseDto.setPesel(citizen.getPesel());
+
+        return citizenUpdateResponseDto;
     }
 
 }
