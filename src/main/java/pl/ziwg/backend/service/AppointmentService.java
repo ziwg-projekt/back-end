bc6d9c9918b7af256fd01addf55846f175e1d27b
Krystian Kania <242014@student.pwr.edu.pl>
2021-04-19 00:02:28 +0200

    Add data transfer object for creating appointments

--- src/main/java/pl/ziwg/backend/service/AppointmentService.java
+++ src/main/java/pl/ziwg/backend/service/AppointmentService.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import pl.ziwg.backend.model.entity.Address;
+import pl.ziwg.backend.dto.AppointmentDto;
+import pl.ziwg.backend.exception.ResourceNotFoundException;
 import pl.ziwg.backend.model.entity.Appointment;
+import pl.ziwg.backend.model.entity.Citizen;
+import pl.ziwg.backend.model.entity.Doctor;
+import pl.ziwg.backend.model.entity.Vaccine;
+import pl.ziwg.backend.model.enumerates.AppointmentState;
 import pl.ziwg.backend.model.repository.AppointmentRepository;
 
 import java.util.List;
@@ -14,10 +19,17 @@ import java.util.Optional;
 @Service
 public class AppointmentService {
     private AppointmentRepository appointmentRepository;
+    private VaccineService vaccineService;
+    private CitizenService citizenService;
+    private DoctorService doctorService;
 
     @Autowired
-    public AppointmentService(AppointmentRepository appointmentRepository) {
+    public AppointmentService(AppointmentRepository appointmentRepository, VaccineService vaccineService,
+                              CitizenService citizenService, DoctorService doctorService) {
         this.appointmentRepository = appointmentRepository;
+        this.vaccineService = vaccineService;
+        this.citizenService = citizenService;
+        this.doctorService = doctorService;
     }
 
     public Page<Appointment> findAllFromPage(Pageable pageable){
@@ -33,6 +45,25 @@ public class AppointmentService {
     }
 
     public Appointment save(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    public Appointment saveDto(final AppointmentDto appointmentDto) {
+        appointmentDto.setState(AppointmentState.CONFIRMED);
+        final Vaccine vaccine = vaccineService.findByCode(appointmentDto.getVaccineCode())
+                .orElseThrow(() -> new ResourceNotFoundException(appointmentDto.getVaccineCode(), "vaccine"));
+        final Citizen citizen = citizenService.findByPesel(appointmentDto.getCitizenPesel())
+                .orElseThrow(() -> new ResourceNotFoundException(appointmentDto.getCitizenPesel(), "pesel"));
+        final Doctor doctor = doctorService.findById(appointmentDto.getDoctorId())
+                .orElseThrow(() -> new ResourceNotFoundException(appointmentDto.getDoctorId(), "doctorId"));
+
+        final Appointment appointment = new Appointment();
+        appointment.setState(appointmentDto.getState());
+        appointment.setVaccine(vaccine);
+        appointment.setCitizen(citizen);
+        appointment.setDoctor(doctor);
+        appointment.setDate(appointmentDto.getDate());
+
         return appointmentRepository.save(appointment);
     }
 
