9d679be6d9481964ef88e8bdd56160ab01b79cd3
Krystian Kania <242014@student.pwr.edu.pl>
2021-05-21 23:26:31 +0200

    Add endpoint to add hospitals by admin

--- src/main/java/pl/ziwg/backend/service/HospitalService.java
+++ src/main/java/pl/ziwg/backend/service/HospitalService.java
@@ -1,31 +1,64 @@
 package pl.ziwg.backend.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import pl.ziwg.backend.model.entity.*;
+import pl.ziwg.backend.BackendApplication;
+import pl.ziwg.backend.dto.HospitalDto;
+import pl.ziwg.backend.exception.UserAlreadyRegisteredException;
+import pl.ziwg.backend.model.entity.Address;
+import pl.ziwg.backend.model.entity.Company;
+import pl.ziwg.backend.model.entity.Hospital;
+import pl.ziwg.backend.model.entity.User;
+import pl.ziwg.backend.model.entity.Vaccine;
 import pl.ziwg.backend.model.repository.HospitalRepository;
+import pl.ziwg.backend.model.repository.UserRepository;
 
+import javax.validation.constraints.NotNull;
 import java.util.*;
 
 @Service
 public class HospitalService {
+    protected static final Logger log = Logger.getLogger(BackendApplication.class);
+
     private HospitalRepository hospitalRepository;
     private CompanyService companyService;
     private VaccineService vaccineService;
+    private UserService userService;
+    private UserRepository userRepository;
 
     @Autowired
     public HospitalService(HospitalRepository hospitalRepository,
                            CompanyService companyService,
-                           VaccineService vaccineService) {
+                           VaccineService vaccineService,
+                           UserService userService,
+                           UserRepository userRepository) {
         this.hospitalRepository = hospitalRepository;
         this.companyService = companyService;
         this.vaccineService = vaccineService;
+        this.userService = userService;
+        this.userRepository = userRepository;
     }
 
+    public Hospital save(@NotNull final HospitalDto newHospital) {
+        if (userService.checkIfUserExists(newHospital.getUsername())) {
+            log.info("User (type hospital) '" + newHospital.getUsername() + "' was found in system");
+            final User user = userRepository.findByUsername(newHospital.getUsername()).get();
+            throw new UserAlreadyRegisteredException(String.format("User with username %s already exist",
+                    user.getUsername()));
+        }
 
-    public Hospital save(Hospital hospital) {
+        final Address address = new Address(newHospital.getCity(), newHospital.getStreet(), newHospital.getStreetNumber());
+        final Hospital hospital = new Hospital();
+        hospital.setAddress(address);
+        hospital.setName(newHospital.getName());
+        userService.saveHospital(newHospital.getUsername(), newHospital.getPassword(), hospital);
+        final User user = userRepository.findByUsername(newHospital.getUsername()).get();
+        hospital.setUser(user);
+        log.info(String.format("Hospital with name %s was created", hospital.getName()));
+
         return hospitalRepository.save(hospital);
     }
 
