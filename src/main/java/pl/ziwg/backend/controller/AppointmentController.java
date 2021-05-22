38f7d5b58cd2e8dcb41a71309ded6a88cb3d21c3
Krystian Kania <242014@student.pwr.edu.pl>
2021-05-22 22:35:29 +0200

    Change param on Dto

--- src/main/java/pl/ziwg/backend/controller/AppointmentController.java
+++ src/main/java/pl/ziwg/backend/controller/AppointmentController.java
@@ -1,6 +1,5 @@
 package pl.ziwg.backend.controller;
 
-import org.hibernate.validator.constraints.pl.PESEL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,14 +7,13 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.ziwg.backend.dto.HospitalEnrollDto;
 import pl.ziwg.backend.dto.VaccineDto;
 import pl.ziwg.backend.exception.ResourceNotFoundException;
 import pl.ziwg.backend.exception.UserTypeException;
@@ -26,7 +24,6 @@ import pl.ziwg.backend.model.enumerates.AppointmentState;
 import pl.ziwg.backend.model.enumerates.UserType;
 import pl.ziwg.backend.model.enumerates.VaccineState;
 import pl.ziwg.backend.notificator.email.EmailSubject;
-import pl.ziwg.backend.security.jwt.service.UserPrinciple;
 import pl.ziwg.backend.service.AppointmentService;
 import pl.ziwg.backend.service.CitizenService;
 import pl.ziwg.backend.service.EmailService;
@@ -34,7 +31,6 @@ import pl.ziwg.backend.service.UserService;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/appointments")
@@ -98,9 +94,9 @@ public class AppointmentController {
     @PreAuthorize("hasRole('HOSPITAL')")
     @PatchMapping("/{id}/hospital/actions/enroll")
     public ResponseEntity<Appointment> enrollByHospital(@PathVariable final Long id,
-                                                        @RequestParam @PESEL final String pesel) {
-        final Citizen citizen = citizenService.findByPesel(pesel)
-                .orElseThrow(() -> new ResourceNotFoundException(pesel, "pesel"));
+                                                        @RequestBody final HospitalEnrollDto hospitalEnrollDto) {
+        final Citizen citizen = citizenService.findByPesel(hospitalEnrollDto.getPesel())
+                .orElseThrow(() -> new ResourceNotFoundException(hospitalEnrollDto.getPesel(), "pesel"));
         final User user = userService.findById(citizen.getUser().getId())
                 .orElseThrow(() -> new ResourceNotFoundException(citizen.getUser().getId(), "user"));
         if (UserType.CITIZEN != user.getUserType()) {
