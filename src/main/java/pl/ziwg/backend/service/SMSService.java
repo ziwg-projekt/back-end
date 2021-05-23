3991390487e0088e96541bcd5555ab47df1c7acc
Krystian Kania <242014@student.pwr.edu.pl>
2021-05-23 18:37:34 +0200

    Add method to sms service

--- src/main/java/pl/ziwg/backend/service/SMSService.java
+++ src/main/java/pl/ziwg/backend/service/SMSService.java
@@ -2,7 +2,11 @@ package pl.ziwg.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.ziwg.backend.model.entity.Address;
+import pl.ziwg.backend.model.entity.Company;
 import pl.ziwg.backend.notificator.sms.SMSSender;
+
+import java.time.LocalDateTime;
 
 @Service
 public class SMSService {
@@ -27,5 +31,25 @@ public class SMSService {
     public void sendVaccinationDate(String number, String vaccinationDate) {
         String message = String.format("Termin szczepienia: %s", vaccinationDate);
         smsSender.sendMessage(number, message);
+    }
+
+    public void sendAppointmentConfirmation(String number, LocalDateTime vaccinationDate, Address hospitalAddress,
+                                            Company vaccineCompany) {
+        String message = String.format(
+                "Udało się zapisać na szczepienie! Wybrano szczepionkę firmy %s. " +
+                "Szczepienie odbędzie się %s w szpitalu pod adresem: ul. %s %s, %s",
+                vaccineCompany.getName(),
+                parseDate(vaccinationDate),
+                hospitalAddress.getStreet(),
+                hospitalAddress.getStreetNumber(),
+                hospitalAddress.getCity()
+        );
+
+        smsSender.sendMessage(number, message);
+    }
+
+    private String parseDate(LocalDateTime time) {
+        return String.format("%d.%d.%d %d:%d", time.getDayOfMonth(), time.getMonthValue(), time.getYear(),
+                time.getHour(), time.getMinute());
     }
 }
